b97b49a6b2ce77d4ab6d83820380cf91b37d4d62
monakordi <mona.kordi68@gmail.com>
2023-06-17 09:47:21 +0330

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -44,9 +44,20 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "7e3ee14cebaec2c9539ff301f396fd91";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`
-
-console.log(apiUrl);
+function search(city) {
+    let apiKey = "7e3ee14cebaec2c9539ff301f396fd91";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
